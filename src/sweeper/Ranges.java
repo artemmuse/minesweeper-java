26a41de819e7223fc6bfc321b070739882da0188
artemmuse <artemuse.it@gmail.com>
2018-07-03 21:07:39 +0300

    Commit #1

---
+++ src/sweeper/Ranges.java
@@ -0,0 +1,29 @@
+package sweeper;
+
+import java.util.ArrayList;
+
+public class Ranges {
+    private static Coord size;
+    private static ArrayList<Coord> allCoord;
+
+    public static void setSize (Coord _size){
+        size = _size;
+        allCoord = new ArrayList<Coord>();
+        for (int y = 0; y < size.y; y++)
+            for (int x = 0; x < size.x; x++)
+                allCoord.add(new Coord(x,y));
+    }
+
+    public static Coord getSize() {
+        return size;
+    }
+
+    public static ArrayList<Coord> getAllCoord (){
+        return allCoord;
+    }
+
+    static boolean inRange(Coord coord){
+        return coord.x >= 0 && coord.x < size.x &&
+               coord.y >= 0 && coord.y < size.y;
+    }
+}
